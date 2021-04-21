b08e52664ff60aca3ada5de0ddd59aa3d1706ceb
Lucas <lucasfonsecard@icloud.com>
2021-04-21 15:44:20 -0300

    update restaurant schema

--- lib/inmana/restaurant.ex
+++ lib/inmana/restaurant.ex
@@ -6,6 +6,10 @@ defmodule Inmana.Restaurant do
   # Set our schema primary key to be and autogen. uuid
   @primary_key {:id, :binary_id, autogenerate: true}
 
+  @required_params [:email, :name]
+
+  @derive {Jason.Encoder, only: @required_params ++ [:id]}
+
   # Map migration fields
   schema "restaurants" do
     field :email, :string
@@ -19,8 +23,8 @@ defmodule Inmana.Restaurant do
   def changeset(params) do
     # __MODULE__ is replaced by the module's name
     %__MODULE__{}
-    |> cast(params, [:email, :name])
-    |> validate_required([:email, :name])
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
     |> validate_length(:name, min: 2)
     # "regex" to validate the email
     |> validate_format(:email, ~r/@/)
