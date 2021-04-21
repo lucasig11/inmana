dff424a2ab1991a49a9bd1345b9007e4fcc5ac2f
Lucas <lucasfonsecard@icloud.com>
2021-04-21 20:36:14 -0300

    add show supply method to supplies controller

--- lib/inmana.ex
+++ lib/inmana.ex
@@ -1,7 +1,10 @@
 defmodule Inmana do
-  alias Inmana.Supplies.Create, as: CreateSupply
   alias Inmana.Restaurants.Create, as: CreateRestaurant
+  alias Inmana.Supplies.Create, as: CreateSupply
+  alias Inmana.Supplies.Get, as: ShowSupply
 
   defdelegate create_restaurant(params), to: CreateRestaurant, as: :call
+
   defdelegate create_supply(params), to: CreateSupply, as: :call
+  defdelegate get_supply(params), to: ShowSupply, as: :call
 end
