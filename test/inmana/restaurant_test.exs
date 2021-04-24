b69b0d5519e29f957995bacb1a15b5d9c682dfbc
Lucas <lucasfonsecard@icloud.com>
2021-04-24 01:27:05 -0300

    add tests for the Restaurants module

---
+++ test/inmana/restaurant_test.exs
@@ -0,0 +1,50 @@
+defmodule Inmana.RestaurantsTest do
+  use Inmana.DataCase, async: true
+
+  alias Inmana.{Repo, Restaurant}
+
+  describe "changeset/1" do
+    test "should return a changeset on valid inputs" do
+      params = %{name: "TestRestaurant", email: "test@rest.com"}
+
+      response = Restaurant.changeset(params)
+
+      assert response.valid?
+    end
+
+    test "shouldn't be able to register with an invalid name or e-mail address" do
+      invalid_name = %{name: "1", email: "test@rest.com"}
+      invalid_email = %{name: "ValidName", email: "invalidmail"}
+
+      name_response = Restaurant.changeset(invalid_name)
+      mail_response = Restaurant.changeset(invalid_email)
+
+      assert !name_response.valid?
+      assert !mail_response.valid?
+    end
+
+    test "shouldn't be able to register with empy fields" do
+      invalid_name = %{name: "", email: "test@rest.com"}
+      invalid_email = %{name: "ValidName", email: ""}
+
+      name_response = Restaurant.changeset(invalid_name)
+      mail_response = Restaurant.changeset(invalid_email)
+
+      assert !name_response.valid?
+      assert !mail_response.valid?
+    end
+
+    test "should throw an error on e-mail collision" do
+      user_data = %{name: "TestRestaurant1", email: "test@rest.com"}
+      imposter_data = %{name: "TestRestaurant2", email: "test@rest.com"}
+
+      user = Restaurant.changeset(user_data)
+      imposter = Restaurant.changeset(imposter_data)
+
+      Repo.insert(user)
+
+      assert {:error, changeset} = Repo.insert(imposter)
+      assert {:email, ["has already been taken"]} in errors_on(changeset)
+    end
+  end
+end
